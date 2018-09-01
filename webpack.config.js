7977f79c15bf1d8b2be2e555150143a1e24a22f3
CaiTom <41202067+CaiTom@users.noreply.github.com>
2018-09-01 17:41:12 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: "index.html" },
+			{ from: './app/logs.html', to: "logs.html" },
+			{ from: './app/users.html', to: "users.html" }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['2017'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  },
+  node: {
+    fs: "empty",
+    child_process: "empty"
+  }
+}
